ad114a1efbdc96ebe155b625601b53234b7b025e
savronmoto <savannaskye@gmail.com>
2022-04-13 19:23:15 -0700

    Added temp button to navigate to Detail Screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:bridge/screens/prompt_details.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -16,7 +17,22 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Bridge'),
           ),
-        body: const Center(child: Text('Bridge')),
+        // The below button and navigation is just so I could 
+        // navigate to the Prompt Detail Screen and see it
+        // in the simulator.
+
+        // It can/will be replaced obviously with the Home Screen!!
+        body: Builder(
+          builder: (context) => Center(
+            child: ElevatedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PromptDetailScreen(comments: List<String>.generate(100, (i) => 'Item $i')))
+              ), 
+              child: const Text('Go To Prompt Detail'),
+            )
+          ),
+        )
       )
     );
   }
