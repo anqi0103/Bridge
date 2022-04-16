dc163e89c060bb7aa044c8df9ec9a66a720e6be0
leeda3 <leeda3@oregonstate.edu>
2022-04-16 18:07:04 -0400

    Add firebase web support

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyAMzhZvT55vHqVkCJ8wOz80M23fwKbaE-I",
+        authDomain: "bridge-7759a.firebaseapp.com",
+        projectId: "bridge-7759a",
+        storageBucket: "bridge-7759a.appspot.com",
+        messagingSenderId: "95176891876",
+        appId: "1:95176891876:web:a3b5cd275257037c2a3bf1",
+        measurementId: "G-SLPJ0K93BC"),
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
@@ -13,5 +22,3 @@ Future<void> main() async {
   ]);
   runApp(const App(title: 'Bridge'));
 }
-
-
