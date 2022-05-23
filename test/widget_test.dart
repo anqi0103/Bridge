573941a984137413dd1ba4955e0594793e260acf
Anqi Lu <anqi0103@gmail.com>
2022-05-22 23:43:30 -0700

    refactored Home Screen codes and write a unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,29 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mockito/annotations.dart';
 import 'package:bridge/app.dart';
+import 'package:mockito/mockito.dart';
+import 'widget_test.mocks.dart';
 
+import '../lib/screens/home_screen_prompts.dart';
+
+@GenerateMocks([
+  FirebaseFirestore,
+  CollectionReference,
+  Query,
+  QuerySnapshot,
+  DocumentReference,
+], customMocks: [
+  MockSpec<QueryDocumentSnapshot<Map<String, dynamic>>>()
+])
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App(title: '',));
+    await tester.pumpWidget(const App(
+      title: '',
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -26,5 +42,28 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  test('returns prompts if the http call completes successfully', () async {
+    final client = MockFirebaseFirestore();
+    final collectionReference = MockCollectionReference<Map<String, dynamic>>();
+    final query = MockQuery<Map<String, dynamic>>();
+    final querySnapshot = MockQuerySnapshot<Map<String, dynamic>>();
+    final queryDocumentSnapshot = MockQueryDocumentSnapshot();
+    final documentReference = MockDocumentReference<Map<String, dynamic>>();
+    final collectionReference2 = MockCollectionReference<Map<String, dynamic>>();
+    final querySnapshot2 = MockQuerySnapshot<Map<String, dynamic>>();
+
+    when(client.collection('tempPrompts')).thenReturn(collectionReference);
+    when(collectionReference.where('timestamp', isEqualTo: anything))
+        .thenReturn(query);
+    when(query.get()).thenAnswer((_) async => querySnapshot);
+    when(querySnapshot.docs).thenReturn(List.from([queryDocumentSnapshot]));
+    when(queryDocumentSnapshot.reference).thenReturn(documentReference);
+    when(documentReference.collection('prompts')).thenReturn(collectionReference2);
+    when(collectionReference2.get()).thenAnswer((_) async => querySnapshot2);
+    when(querySnapshot2.docs).thenReturn(List.from([]));
+
+    expect(await generatePromptsIfNone(client), List.from([]));
   });
 }
